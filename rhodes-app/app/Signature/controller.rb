0c71c4650c7fc821e4a1bfbe8fd5671b456e2e5e
Javier Molina <javier@anatomicsoft.com>
2013-04-25 19:50:01 +0200

    Add sample templates / structure

---
+++ rhodes-app/app/Signature/controller.rb
@@ -0,0 +1,13 @@
+require 'rho'
+require 'rho/rhocontroller'
+require 'rho/rhoerror'
+require 'helpers/browser_helper'
+
+class SignatureController < Rho::RhoController
+  include BrowserHelper
+  
+  def index
+    render
+  end
+  
+end
