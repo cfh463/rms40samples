d85ca042060d27858b0b650203c9124e0e0a9b6a
Javier Molina <javier@anatomicsoft.com>
2013-04-29 12:31:45 +0200

    Begin work on Barcode examples

--- rhodes-app/app/Barcode/controller.rb
+++ rhodes-app/app/Barcode/controller.rb
@@ -10,4 +10,12 @@ class BarcodeController < Rho::RhoController
     render
   end
   
+  def scan_using_default_scanner
+    # TODO: new Barcode API not implemented in Ruby - only old API is present
+    Barcode.take_barcode(url_for(:action => :scan_received), {})
+  end
+  
+  def scan_received
+    Rho::Log.info(@params.to_s)
+  end
 end
