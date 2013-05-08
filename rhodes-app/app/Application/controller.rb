8f389efced7196b13b405ac7211dd62d4fe4d967
Dhepthi L N <dhepthiln@spritle.com>
2013-05-08 15:14:47 -0700

    Application API methods and properties implementation

--- rhodes-app/app/Application/controller.rb
+++ rhodes-app/app/Application/controller.rb
@@ -10,4 +10,28 @@ class ApplicationController < Rho::RhoController
     render
   end
   
+  def app_folder 
+    appFolder = Rho::Application.appBundleFolder
+    Alert.show_popup(appFolder)
+    render :action => :wait
+  end
+
+   def set_locale
+  	Rho::Application.setLocale('en')
+  	current_locale = Rho::Application.locale
+  	Alert.show_popup(current_locale)
+  	render :action => :wait
+   end
+
+   def quit_app 
+    Rho::Application.quit
+   end
+
+  def minimize_app
+  Rho::Application.minimize
+  end
+  
+  def restore_app
+  	Rho::Application.restore
+  end
 end
