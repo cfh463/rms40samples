81d1db5d0b8b71342270c06b23ca68f4720cef2b
Javier Molina <javier@kutirtech.com>
2013-05-11 02:36:37 +0200

    Add cardreader samples

--- rhodes-app/app/Cardreader/controller.rb
+++ rhodes-app/app/Cardreader/controller.rb
@@ -9,5 +9,23 @@ class CardreaderController < Rho::RhoController
   def index
     render
   end
+
+  def connect
+    Rho::CardReader.open({}, url_for(:action => :cardreader_callback))
+    redirect :index
+  end
   
+  def cardreader_callback
+    Alert.show_popup("Received card reader data: #{@params[:data]}")
+  end
+  
+  def set_properties
+    # Configure the MSR to output keystrokes instead of calling a function when a card is swiped
+    # Note the absence of a callback parameter
+    # Also, After emitting the keystrokes, automatically send a "Tab" keypress
+    Rho::CardReader.open({:autoTab => true})
+      
+    # Instead of "Tab", we could request a simulated "Enter" 
+    Rho::CardReader.open({:autoEnter => true})
+  end
 end
