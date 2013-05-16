bfda253f28f48e0d9223c2bafc1a89c84c508485
Javier Molina <javier@kutirtech.com>
2013-05-16 21:20:29 +0200

    Bring more samples into the menu

---
+++ rhodes-app/public/js/samples/cardreader.js
@@ -0,0 +1,25 @@
+KitchenSink.Samples.CardReader = KitchenSink.Samples.CardReader || (function() {
+    function cardreader_callback(params) {
+        alert("Received data from card reader: "+params["data"]);
+      }
+      
+      function connect() {
+        Rho.CardReader.open({}, cardreader_callback);
+      }
+      
+      function setProperties() {
+        // Configure the MSR to output keystrokes instead of calling a function when a card is swiped
+        // Note the absence of a callback parameter
+        // Also, After emitting the keystrokes, automatically send a "Tab" keypress
+
+        Rho.CardReader.open({autoTab : true});
+        
+        Rho.CardReader.open({autoEnter : true});
+      }
+	
+	return {
+		connect : connect,
+		setProperties : setProperties
+	}
+
+})();
