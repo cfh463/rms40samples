c356ba09702398f2e5682e574b2456d7e0ba35c9
Javier Molina <javier@kutirtech.com>
2013-05-17 18:53:56 +0200

    Implement search functionality

--- rhodes-app/public/js/kitchensink.js
+++ rhodes-app/public/js/kitchensink.js
@@ -8,8 +8,24 @@ var KitchenSink = (function() {
 			$(".ui-page-active .example_description").fadeToggle();
 		}
 		
+		function filter_navmenu() {
+			var searchText = $(this).val();
+			var navmenu = $("#navmenu");
+			
+			navmenu.find("div[data-role=collapsible]").each(function() {
+				var keywords = $(this).data('filter');
+				// If the search is empty, show every item
+				if (typeof(keywords)==="undefined" || keywords.indexOf(searchText)!==-1) {
+					$(this).fadeIn();
+				} else {
+					$(this).fadeOut();
+				}
+			});
+		}
+		
 		function init() {
 			$(document).on("click", "a.ks_toggle_example_description", toggle_example_description);
+			$(document).on("change keyup", "#navmenu_searchbox", filter_navmenu);
 		}
 		
 		init();
