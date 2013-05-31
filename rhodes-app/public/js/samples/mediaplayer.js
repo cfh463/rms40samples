a2c85efc0fb48365532e088b9dc958bda7789799
Dhepthi L N <dhepthiln@spritle.com>
2013-05-31 14:28:27 -0700

    MediaPlayer API

---
+++ rhodes-app/public/js/samples/mediaplayer.js
@@ -0,0 +1,11 @@
+KitchenSink.Samples.Mediaplayer = KitchenSink.Samples.Mediaplayer || (function() {
+
+ 	function showRingtones() {
+		alert("Yet to implement")
+	}
+	
+	return {
+		showRingtones: showRingtones
+	};
+
+})();
