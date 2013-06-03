2c5de337c7040bcfa7e4b833f106bc6afb5b3768
Javier Molina <javier@kutirtech.com>
2013-06-03 17:09:41 +0200

    Polish ScreenOrientation samples

--- rhodes-app/app/Screenorientation/controller.rb
+++ rhodes-app/app/Screenorientation/controller.rb
@@ -5,23 +5,19 @@ require 'helpers/browser_helper'
 
 class ScreenorientationController < Rho::RhoController
   include BrowserHelper
-  
+
   def auto_rotate
     Rho::ScreenOrientation.autoRotate=true
     @is_auto_rotated = Rho::ScreenOrientation.autoRotate
   end
   
   def set_callback
-    render
-  end
-  
-  def trigger_callback
-    Rho::ScreenOrientation.upsideDown
-    ScreenOrientation.setScreenOrientationEvent(url_for(:action => :orientation_callback))
+    Rho::ScreenOrientation.setScreenOrientationEvent(url_for(:action => :orientation_callback))
+    redirect :action => :confirm_set_callback
   end
   
   def orientation_callback
-    Alert.show_popup("Screen changed orientation")
+    Alert.show_popup("The screen changed orientation")
   end
   
   def left_handed_position
@@ -39,4 +35,5 @@ class ScreenorientationController < Rho::RhoController
   def normal_position
     Rho::ScreenOrientation.normal
   end
+
 end
