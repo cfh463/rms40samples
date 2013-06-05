5a806f4e7b60aa1f64669ea826847fdb8c57fa07
visnupriya <visnupriyar@spritle.com>
2013-06-05 15:07:28 +0530

    process api initial commit

---
+++ rhodes-app/app/Process/controller.rb
@@ -0,0 +1,21 @@
+require 'rho'
+require 'rho/rhocontroller'
+require 'rho/rhoerror'
+require 'helpers/browser_helper'
+
+class ProcessController < Rho::RhoController
+  include BrowserHelper
+
+  # GET /Process
+  def index
+
+  end
+
+  def launch_external_app
+  	p "launch_external_app---------------------------launch_external_app----------"
+  	Rho::Process.runApplication("Asset Tracker")
+  	p "------------------------sss"
+  	render :action => :index
+  end
+  
+end
