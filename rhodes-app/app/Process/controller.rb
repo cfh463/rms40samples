fe01f007d3c5b1f0480ad1bc6165b516a04f9431
Javier Molina <javier@kutirtech.com>
2013-06-16 17:55:42 +0200

    Fix & integrate Process sample

--- rhodes-app/app/Process/controller.rb
+++ rhodes-app/app/Process/controller.rb
@@ -11,11 +11,31 @@ class ProcessController < Rho::RhoController
 
   end
 
-  def launch_external_app
-  	p "launch_external_app---------------------------launch_external_app----------"
-  	Rho::Process.getProcessExitCode()
-  	p "------------------------sss"
+  def run_app(blocking)
+    app_name = nil
+    params = nil
+    case Rho::System.platform
+    when "WINDOWS"
+      app_name = "iexplore"
+    when "ANDROID"
+      app_name = "com.android.browser"
+      params = "https://launchpad.motorolasolutions.com"
+    when "APPLE"
+      app_name = "itms://"
+    else
+      
+    end
+    
+    Rho::Process.runApplication(app_name,params,blocking)    
+  end
+  
+  def run_blocking
+    run_app(true)
   	render :action => :index
   end
   
+  def run_nonblocking
+    run_app(false)
+    render :action => :index
+  end
 end
