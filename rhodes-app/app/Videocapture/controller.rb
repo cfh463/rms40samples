a57850e18e511ce0695283ef4ecbf82a8ea38223
visnupriya <visnupriyar@spritle.com>
2013-06-28 17:08:56 +0530

    added videocapture.js file

--- rhodes-app/app/Videocapture/controller.rb
+++ rhodes-app/app/Videocapture/controller.rb
@@ -12,26 +12,24 @@ class VideocaptureController < Rho::RhoController
   
   def capture_video
     Rho::Videocapture.fileName = '/RhoVideoCapture'
-    Rho::Videocapture.duration = 10000
-    Rho::Videocapture.start(url_for(:action => :mysaveevent))
+    Rho::Videocapture.duration = 60000
+    Rho::Videocapture.start(url_for(:action => :save_captured_video))
+    redirect :index
   end
 
-  def mysaveevent
+  def save_captured_video
     puts "Save params: #{@params}"
     puts "Result is #{@params['transferResult']}"
     puts "Name is #{@params['fileName']}"       
     puts "Size is #{@params['size']}" 
-    render :action => :index
+    Alert.show_popup("Video was saved in the #{@params['fileName']} ")
   end
   
   def capture_video_with_resolution
     Rho::Videocapture.fileName = '/RhoVideoCapture'
     Rho::Videocapture.resolution = @params['resolution']
-    Rho::Videocapture.duration = 10000
-    Rho::Videocapture.start(url_for(:action => :resolution_callback))
-  end
-
-  def resolution_callback
-    render :action => :select_resolution
+    Rho::Videocapture.duration = 60000
+    Rho::Videocapture.start(url_for(:action => :save_captured_video))
+    redirect :select_resoluation
   end
 end
