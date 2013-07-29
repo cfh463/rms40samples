ba9c4b40a8154f57636c945dc41c3e84e9a22934
Javier Molina <javier@kutirtech.com>
2013-07-29 12:32:23 +0200

    Add working ORM sample

--- rhodes-app/app/ORM/controller.rb
+++ rhodes-app/app/ORM/controller.rb
@@ -7,11 +7,6 @@ class ORMController < Rho::RhoController
   include BrowserHelper
   
   def js_model_menu
-    model = Rhom.getModel("Product")
-    
-    if model
-      @products = model.findAll
-    end
     
     render
   end
